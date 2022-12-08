194a3555b80b7d7a489d34cee879e2b1943b2e1e
euiraekim <harrykur139@gmail.com>
2022-12-08 12:54:15 +0000

    init

---
+++ test.py
@@ -0,0 +1,14 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime
+
+dag = DAG(dag_id="test_dag", start_date=datetime(2022, 12, 7))
+
+t1 = DummyOperator(
+        task_id="dummy_test",
+        dag=dag)
+
+t2 = SSHOperator(
+        task_id='SSHOperator',
+        ssh_conn_id='emr-spark',
+        command='echo "Text from SSH Operator"')
