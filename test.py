32ad704aa4ba9630d33eec3471ca88cf1c54c79d
euiraekim <harrykur139@gmail.com>
2022-12-09 01:51:07 +0000

    ssh 추가

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.contrib.operators.ssh_operator import SSHOperator
 from datetime import datetime
 
-dag = DAG(dag_id="test_dag", start_date=datetime(2022, 12, 7))
+dag = DAG(dag_id="test_dag")
 
 t1 = DummyOperator(
         task_id="dummy_test",
@@ -12,6 +12,6 @@ t1 = DummyOperator(
 t2 = SSHOperator(
         task_id='SSHOperator',
         ssh_conn_id='emr-spark',
-        command='echo "Text from SSH Operator"')
+        command='spark-submit --jars /usr/share/aws/redshift/jdbc/RedshiftJDBC.jar,/usr/share/aws/redshift/spark-redshift/lib/spark-redshift.jar,/usr/share/aws/redshift/spark-redshift/lib/spark-avro.jar,/usr/share/aws/redshift/spark-redshift/lib/minimal-json.jar test.py')
 
 t1 >> t2
