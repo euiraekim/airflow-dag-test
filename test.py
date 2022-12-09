fc91bec50034f9be156e69b32efbf267769b5022
euiraekim <harrykur139@gmail.com>
2022-12-09 02:31:37 +0000

    redshift access

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.contrib.operators.ssh_operator import SSHOperator
 from datetime import datetime
 
-dag = DAG(dag_id="test_dag", start_date = datetime(2022,12,7))
+import redshift_connector
+
+dag = DAG(dag_id="test_dag", start_date = datetime(2022,12,9))
 
 t1 = DummyOperator(
         task_id="dummy_test",
@@ -14,4 +16,23 @@ t2 = SSHOperator(
         ssh_conn_id='emr-spark',
         command='spark-submit --jars /usr/share/aws/redshift/jdbc/RedshiftJDBC.jar,/usr/share/aws/redshift/spark-redshift/lib/spark-redshift.jar,/usr/share/aws/redshift/spark-redshift/lib/spark-avro.jar,/usr/share/aws/redshift/spark-redshift/lib/minimal-json.jar test.py')
 
-t1 >> t2
+def access_redshift():
+    conn = redshift_connector.connect(
+            host='redshift-test.cyernhele58c.ap-northeast-2.redshift.amazonaws.com:5439',
+            database='redshift_test',
+            user='testuser',
+            password='Testpw1234')
+
+    cursor = conn.cursor()
+    cursor.execute("select * from users")
+    result = cursor.fetchall()
+    print('result : ', result)
+
+
+t3 = PythonOperator(
+            task_id = 'access_redshift', 
+            python_callable = access_redshift,
+            dag = dag
+            )
+
+t1 >> t3
