2f3c514ecec65ebfc23ecbcff6c77a2a99e1532f
euiraekim <harrykur139@gmail.com>
2022-12-10 11:42:15 +0000

    asdf

--- test2.py
+++ test2.py
@@ -29,7 +29,7 @@ def get_postgresql_conn():
             print(e)
 
 def query(ds):
-    insert_query = "insert into sign_up_count values ('{}');".format('ds')
+    insert_query = f"insert into sign_up_count values ('{ds}');"
 
     select_query = """
         select * from sign_up_count;
@@ -48,7 +48,7 @@ def query(ds):
 t3 = PythonOperator(
             task_id = 'query', 
             python_callable = query,
-            op_args=['{{ ds }}'],
+            op_kwargs={ 'ds': '{{ ds }}' },
             dag = dag
             )
 
