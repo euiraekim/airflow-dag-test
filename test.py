617a1c4be2cca11d097a1e9d0c94725f236e43b5
euiraekim <harrykur139@gmail.com>
2022-12-10 10:46:45 +0000

    asdf

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from airflow.operators.python import PythonOperator
 from airflow.contrib.operators.ssh_operator import SSHOperator
 from datetime import datetime
 
+import psycopg2
+
 import redshift_connector
 
 dag = DAG(dag_id="test_dag", start_date = datetime(2022,12,9))
