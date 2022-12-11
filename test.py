ebfe493bd572c5e8648f4daea9081ac3942f624e
euiraekim <harrykur139@gmail.com>
2022-12-11 01:31:22 +0000

    aa

--- test.py
+++ test.py
@@ -2,22 +2,24 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python import PythonOperator
 from airflow.contrib.operators.ssh_operator import SSHOperator
+
 from datetime import datetime
-
 import psycopg2
-
 import redshift_connector
 
-dag = DAG(dag_id="test_dag", start_date = datetime(2022,12,9))
+dag = DAG(dag_id="order_processing",
+        start_date = datetime(2022, 12, 11, 12),
+        schedule_interval = '30 * * * *',)
 
-t1 = DummyOperator(
-        task_id="dummy_test",
-        dag=dag)
+start_task = DummyOperator(
+                task_id="start",
+                dag=dag)
 
-t2 = SSHOperator(
-        task_id='SSHOperator',
+
+spark_task = SSHOperator(
+        task_id='spark-s3-to-redshift',
         ssh_conn_id='emr-spark',
-        command='spark-submit --jars /usr/share/aws/redshift/jdbc/RedshiftJDBC.jar,/usr/share/aws/redshift/spark-redshift/lib/spark-redshift.jar,/usr/share/aws/redshift/spark-redshift/lib/spark-avro.jar,/usr/share/aws/redshift/spark-redshift/lib/minimal-json.jar test.py')
+        command='spark-submit --jars /usr/share/aws/redshift/jdbc/RedshiftJDBC.jar,/usr/share/aws/redshift/spark-redshift/lib/spark-redshift.jar,/usr/share/aws/redshift/spark-redshift/lib/spark-avro.jar,/usr/share/aws/redshift/spark-redshift/lib/minimal-json.jar users_to_redshift.py')
 
 def access_redshift():
     conn = redshift_connector.connect(
@@ -38,4 +40,4 @@ t3 = PythonOperator(
             dag = dag
             )
 
-t1 >> t3
+start_task >> spark_task
