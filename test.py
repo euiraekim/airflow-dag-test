29cb005987e45e4c10eac1fb412e890891ceb69f
euiraekim <harrykur139@gmail.com>
2022-12-11 03:54:03 +0000

    aa

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ start_task = DummyOperator(
                 dag=dag)
 
 
-dt = {{ ts.strftime('%Y-%m-%d %H:%M:%S') }}
+#dt = {{ ts.strftime('%Y-%m-%d %H:%M:%S') }}
 
 spark_task = SSHOperator(
         task_id='spark-s3-to-redshift',
