efbd74ab638ca4f73ded7d421d251f0e1df50110
euiraekim <harrykur139@gmail.com>
2022-12-11 03:31:37 +0000

    aa

--- test2.py
+++ test2.py
@@ -59,7 +59,7 @@ dt = datetime.now()
 t3 = PythonOperator(
             task_id = 'query', 
             python_callable = query,
-            op_kwargs={ 'dt': "{{ dt }}") },
+            op_kwargs={ 'dt': "{{ ds }}") },
             dag = dag
             )
 
