a97a35d819595aa705df4f60d297e70582c6a055
euiraekim <harrykur139@gmail.com>
2022-12-11 07:32:34 +0000

    aa

--- functions.py
+++ functions.py
@@ -1,14 +1,39 @@
 import redshift_connector
+import psycopg2
+
+def get_redshift_conn():
+    try:
+        conn = redshift_connector.connect(
+            host='redshift-test.cyernhele58c.ap-northeast-2.redshift.amazonaws.com',
+            database='redshift_test',
+            user='testuser',
+            password='Testpw1234')
+        conn.autocommit = True
+        return conn
+    except Exception as e:
+        print(e)
+
+def get_postgresql_conn():
+    try:
+        dbname = 'postgresql'
+        host = 'postgresql.cu8vcr2xeenz.ap-northeast-2.rds.amazonaws.com'
+        port = 5432
+        user = 'testuser'
+        pw = 'Testpw1234'
+        conn = psycopg2.connect(f'dbname={dbname} host={host} port={port} user={user} password={pw}')
+        conn.autocommit = True
+        return conn
+    except Exception as e:
+        print(e)
 
 
 def access_redshift():
-    conn = redshift_connector.connect(
-        host='redshift-test.cyernhele58c.ap-northeast-2.redshift.amazonaws.com',
-        database='redshift_test',
-        user='testuser',
-        password='Testpw1234')
+    conn = get_postgresql_conn()
 
-    cursor = conn.cursor()
-    cursor.execute("insert into test values (3, 'cc')")
-    #result = cursor.fetchall()
-    #print('result : ', result)
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute("insert into users values (3, 'cc')")
+            #rows = cursor.fetchall()
+            #print('rows'+ str(rows))
+        except Exception as e:
+            print(e)
