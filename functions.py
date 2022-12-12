fdd53c22012084d43fe219386643efdcd6b48f9a
euiraekim <harrykur139@gmail.com>
2022-12-12 10:29:33 +0000

    a

--- functions.py
+++ functions.py
@@ -1,6 +1,31 @@
-from functions import get_redshift_conn, get_postgresql_conn
-
 from datetime import datetime, timedelta
+import redshift_connector
+import psycopg2
+
+def get_redshift_conn():
+    try:
+        conn = redshift_connector.connect(
+                    host='redshift-test.cyernhele58c.ap-northeast-2.redshift.amazonaws.com',
+                    database='redshift_test',
+                    user='testuser',
+                    password='Testpw1234')
+        conn.autocommit = True
+        return conn
+    except Exception as e:
+        print(e)
+
+def get_postgresql_conn():
+    try:
+        dbname = 'postgresql'
+        host = 'postgresql.cu8vcr2xeenz.ap-northeast-2.rds.amazonaws.com'
+        port = 5432
+        user = 'testuser'
+        pw = 'Testpw1234'
+        conn = psycopg2.connect(f'dbname={dbname} host={host} port={port} user={user} password={pw}')
+        conn.autocommit = True
+        return conn
+    except Exception as e:
+        print(e)
 
 
 def insert_query(query, db):
