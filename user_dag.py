80771262329b9189a82b9367b1f7dd36a888f091
euiraekim <harrykur139@gmail.com>
2022-12-12 10:22:06 +0000

    a

--- user_dag.py
+++ user_dag.py
@@ -4,16 +4,19 @@ from airflow.operators.python import PythonOperator
 from airflow.contrib.operators.ssh_operator import SSHOperator
 
 from datetime import datetime
-import psycopg2
-import redshift_connector
-from functions import access_redshift
+from functions import (
+            user_count_task,
+            user_address_count_task,
+            user_email_domain_count_task,
+            user_gender_count_task
+        )
 
 dag = DAG(dag_id="user_processing",
         start_date = datetime(2022, 12, 12, 6),
         schedule_interval = '5 * * * *',)
 
 start_task = DummyOperator(
-                task_id="start",
+                task_id="start_task",
                 dag=dag)
 
 
@@ -25,10 +28,38 @@ spark_task = SSHOperator(
         command=f'spark-submit --jars /usr/share/aws/redshift/jdbc/RedshiftJDBC.jar,/usr/share/aws/redshift/spark-redshift/lib/spark-redshift.jar,/usr/share/aws/redshift/spark-redshift/lib/spark-avro.jar,/usr/share/aws/redshift/spark-redshift/lib/minimal-json.jar /home/hadoop/data-pipeline-with-aws/spark/users_to_redshift.py -dt "{dt}"')
 
 
-t3 = PythonOperator(
-            task_id = 'access_redshift', 
-            python_callable = access_redshift,
+user_count_task = PythonOperator(
+            task_id = 'user_count_task', 
+            python_callable = user_count_task,
+            op_kwargs={ 'execution_date': dt }
             dag = dag
             )
 
-start_task >> spark_task
+user_address_count_task = PythonOperator(
+            task_id = 'user_address_count_task', 
+            python_callable = user_address_count_task,
+            op_kwargs={ 'execution_date': dt }
+            dag = dag
+            )
+
+user_email_domain_count_task = PythonOperator(
+            task_id = 'user_email_domain_count_task', 
+            python_callable = user_email_domain_count_task,
+            op_kwargs={ 'execution_date': dt }
+            dag = dag
+            )
+
+user_gender_count_task = PythonOperator(
+            task_id = 'user_gender_count_task', 
+            python_callable = user_gender_count_task,
+            op_kwargs={ 'execution_date': dt }
+            dag = dag
+            )
+
+end_task = DummyOperator(
+                task_id="end_task",
+                dag=dag)
+
+start_task >> spark_task >> [
+        user_count_task, user_address_count_task,user_email_domain_count_task, user_gender_count_task
+        ] >> end_task
